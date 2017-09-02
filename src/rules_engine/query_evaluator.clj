016b260e34bfa5c4d32cb7ebbae974905366a50c
Manuel Porto <manu.porto94@gmail.com>
2017-09-02 20:20:30 -0300

    Delete unused file

--- src/rules_engine/query_evaluator.clj
+++
@@ -1,7 +0,0 @@
-(ns rules_engine.query-evaluator
-  (:use [rules-engine.parser :only [get-facts]]))
-
-(defn evaluate-query
-  [database query]
-  (let [facts (get-facts database)]
-    (.contains facts query)))
