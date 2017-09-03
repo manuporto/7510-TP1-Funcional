8e6fa71e74563a9a31a658bf463341b71c4a50b1
Manuel Porto <manu.porto94@gmail.com>
2017-09-03 15:17:38 -0300

    Refactor

--- src/rules_engine/parser.clj
+++ src/rules_engine/parser.clj
@@ -1,5 +1,6 @@
 (ns rules-engine.parser
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [rules-engine.utils :as utils]))
 
 (defn valid-fact?
   "Checks if a single fact it's valid."
@@ -23,11 +24,7 @@
   [database]
   (not (.contains (map (fn [x] (or (valid-fact? x) (valid-rule? x)))  database) false)))
 
-(defn trim-whitespace-and-newlines
-  [string-to-trim]
-  (str/trim-newline (str/trim string-to-trim)))
-
-(defn parse-facts
+(defn parse-database
   "Receives a database (string) containing facts and rules in the form of 
   'male(manuel).
   male(juan).
@@ -35,11 +32,11 @@
   son(X,Y) :- male(X), father(Y, X).
   ' and returns a set containing only the facts"
   [database]
-  (set (filter (fn [x] (not (str/blank? x))) (map trim-whitespace-and-newlines (str/split database  #"\.")))))
+  (set (filter utils/not-blank? (map utils/trim-whitespace-and-newlines (str/split database  #"\.")))))
 
 (defn get-facts
   [facts]
-  (let [parsed-facts (parse-facts facts)]
+  (let [parsed-facts (parse-database facts)]
     (if (.contains (map valid-fact? parsed-facts) false)
     (set nil)
     parsed-facts)))
@@ -51,7 +48,7 @@
 
 (defn get-database
   [raw-database]
-  (let [parsed-database (parse-facts raw-database)]
+  (let [parsed-database (parse-database raw-database)]
     (if (valid-database? parsed-database)
       (load-database parsed-database)
       {})))
