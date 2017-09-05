532922fd9fed62df20c915b445950d1b94dac89f
Manuel Porto <manu.porto94@gmail.com>
2017-09-05 18:31:22 -0300

    Refactor

--- src/rules_engine/parser/fact_parser.clj
+++ src/rules_engine/parser/fact_parser.clj
@@ -9,12 +9,27 @@
   ; Example: father(john, max)
   (not (= nil (re-matches #"^[a-z]+\(([a-z]+, )*[a-z]+\)" fact))))
 
+(defn get-fact-name
+  "Receive a string containing a raw fact and return it's name"
+  [fact]
+  (subs fact 0 (str/index-of fact "(")))
+
+(defn get-fact-args
+  "Receive a string containing a raw fact and return it's arguments as a list"
+  [fact]
+  (str/split (subs fact (inc (str/index-of fact "(")) (str/index-of fact ")")) #", "))
+
 (defn create-entity-fact
   "Receive a raw string containing a fact and create a Fact entity from it."
   [fact]
-  (let [name (subs fact 0 (str/index-of fact "("))
-        args (str/split (subs fact (inc (str/index-of fact "(")) (str/index-of fact ")")) #", ")]
+  (let [name (get-fact-name fact)
+        args (get-fact-args fact)]
         (new-fact name args)))
+
+(defn create-raw-fact
+  "Receive a Fact entity and return a string containing a raw fact"
+  [fact]
+  (str (:name fact) "(" (str/join ", " (:args fact)) ")"))
 
 ; TODO refactor or delete
 ;(defn get-facts
